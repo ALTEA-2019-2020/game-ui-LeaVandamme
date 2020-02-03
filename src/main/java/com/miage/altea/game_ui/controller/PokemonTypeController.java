b38c930b2b3491aaee57a8deb96eac9081e56fa8
Lea Vandamme <you@example.com>
2020-02-03 12:22:42 +0100

    last part

---
+++ src/main/java/com/miage/altea/game_ui/controller/PokemonTypeController.java
@@ -0,0 +1,27 @@
+package com.miage.altea.game_ui.controller;
+
+import com.miage.altea.game_ui.pokemonTypes.service.PokemonTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PokemonTypeController {
+
+    @Autowired
+    private PokemonTypeService pokemonTypeService;
+
+    public void setPokemonTypeService(PokemonTypeService pokemonTypeService){
+        this.pokemonTypeService = pokemonTypeService;
+    }
+
+    @GetMapping("/pokedex")
+    public ModelAndView pokedex(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("pokedex");
+        mav.addObject("pokemonTypes", pokemonTypeService.listPokemonsTypes());
+        return mav;
+    }
+
+}
